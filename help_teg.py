0ba43909427e628e0196613eda8cf175a574dfef
Eazyname <86982708+Eazyname@users.noreply.github.com>
2022-04-01 22:51:49 +0300

    Update help_teg.py

--- help_teg.py
+++ help_teg.py
@@ -7,12 +7,9 @@
 Rus_News = {
      "www.gazeta.ru": ".b_article-text p",
           "lenta.ru": ".topic-body__content-text",
-        "www.rbc.ru": "p",
             "ria.ru": ".article__text",
       "www.vesti.ru": ".article__text p",
            "news.ru": ".post-wrapper p",
-   "news.rambler.ru": "p",
-        "smotrim.ru": ".article__body p",
-   "www.interfax.ru": "p"
+        "smotrim.ru": ".article__body p"
 }
 
