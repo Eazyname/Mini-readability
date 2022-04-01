59ea56d15c05e14701c72cc143eb30244bef802c
Eazyname <86982708+Eazyname@users.noreply.github.com>
2022-04-01 21:39:01 +0300

    Add files via upload

---
+++ help_teg.py
@@ -0,0 +1,18 @@
+"""
+Даный словарь создан для настройки и упрощения поиска
+нужного текста с рализных новостыных медия
+испольуеются CSS селекторы
+"""
+
+Rus_News = {
+     "www.gazeta.ru": ".b_article-text p",
+          "lenta.ru": ".topic-body__content-text",
+        "www.rbc.ru": "p",
+            "ria.ru": ".article__text",
+      "www.vesti.ru": ".article__text p",
+           "news.ru": ".post-wrapper p",
+   "news.rambler.ru": "p",
+        "smotrim.ru": ".article__body p",
+   "www.interfax.ru": "p"
+}
+
